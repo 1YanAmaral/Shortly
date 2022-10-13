2a5e93ced2ce50681fc53d08b1f207ae542f4463
Yan Amaral <yan@pop-os.localdomain>
2022-10-13 10:21:42 -0300

    feat: get url by id & redirect to url

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -19,3 +19,49 @@ export async function postUrl(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function getUrl(req, res) {
+  const urlId = req.params.id;
+
+  try {
+    const url = await connection.query(
+      `SELECT id, "shortUrl", url FROM urls WHERE urls.id = $1;`,
+      [urlId]
+    );
+
+    if (url.rowCount === 0) {
+      return res.sendStatus(404);
+    }
+
+    res.status(200).send(url.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
+}
+
+export async function openUrl(req, res) {
+  const shortUrl = req.params.shortUrl;
+
+  try {
+    const validUrl = await connection.query(
+      `SELECT id, url FROM urls WHERE urls."shortUrl" = $1;`,
+      [shortUrl]
+    );
+    if (validUrl.rowCount === 0) {
+      return res.sendStatus(404);
+    }
+
+    const urlId = validUrl.rows[0].id;
+    const fullUrl = validUrl.rows[0].url;
+
+    await connection.query(`INSERT INTO visits ("urlId") VALUES ($1);`, [
+      urlId,
+    ]);
+
+    res.send(`Redirecionei para ${fullUrl}`);
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
+}
--- src/routers/urlRouter.js
+++ src/routers/urlRouter.js
@@ -1,9 +1,11 @@
-import { postUrl } from "../controllers/urlController.js";
+import { postUrl, getUrl, openUrl } from "../controllers/urlController.js";
 import express from "express";
 import validateUrl from "../middlewares/urlValidation.js";
 
 const urlRouter = express.Router();
 
 urlRouter.post("/urls/shorten", validateUrl, postUrl);
+urlRouter.get("/urls/:id", getUrl);
+urlRouter.get("/urls/open/:shortUrl", openUrl);
 
 export default urlRouter;
